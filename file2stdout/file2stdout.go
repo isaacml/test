373231b9dca782d31dc840914004495d120887c2
todostreaming <info@todostreaming.es>
2016-04-27 19:42:08 +0200

    file2stdout better code

--- file2stdout/file2stdout.go
+++ file2stdout/file2stdout.go
@@ -12,26 +12,27 @@ func main(){
 func dumpfile (filename string){
 	fr,err := os.Open(filename)
 	defer fr.Close()
-	if err == nil{
-//		reader := bufio.NewReader(fr)
-		data := make([]byte, MB1)
-		for{
-			data = data[:cap(data)]
-			n, err := fr.Read(data)
-			fmt.Fprintf(os.Stderr, "Bytes read: %d\n", n)
-	        if err != nil {
-	            if err == io.EOF { 
-	            	fmt.Fprintln(os.Stderr, "END OF FILE\n")
-	            	break 
-	           	}
-	            fmt.Fprintln(os.Stderr, err)
-	            return
-        	}
-       		data = data[:n]
-       		writer := bufio.NewWriter(os.Stdout)
-			writer.Write(data)
-			writer.Flush()
-		}
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Open file error\n")
+	}
+	//reader := bufio.NewReader(fr)
+	data := make([]byte, MB1)
+	for{
+		data = data[:cap(data)]
+		n, err := fr.Read(data)
+        if err != nil {
+ 			if err != io.EOF {
+ 				fmt.Fprintln(os.Stderr, err)
+ 				return
+ 			}
+           	fmt.Fprintln(os.Stderr, "END OF FILE\n")
+           	break 
+       	}
+		fmt.Fprintf(os.Stderr, "Bytes read: %d\n", n)
+   		data = data[:n]
+   		writer := bufio.NewWriter(os.Stdout)
+		writer.Write(data)
+		writer.Flush()
 	}
 	fmt.Fprintln(os.Stderr, "END OF DUMP\n")
 }
