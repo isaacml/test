3049789d44c44ebb61946428f6bbab0ccecd7910
Isaac ML <isaac@todostreaming.es>
2016-06-14 12:13:12 +0200

    Se añaden nuevas cosas

--- statsLive/live.go
+++ statsLive/live.go
@@ -4,21 +4,25 @@ import (
 	"strings"
 	"fmt"
 )
+
 func main(){
-	userAgent := map[string]string {"win":"Windows", "mac":"Mac OS X", "and":"Android", "lin":"Linux"}
-	existe 	  := false
-	username  := "isaac"
 	namefile  := "/var/segments/live/stream.m3u8"
 	agent 	  := "Mozilla/5.0 (X11; Linux x86_64; rv:38.0) Gecko/20100101 Firefox/38.0 Iceweasel/38.6.1"
 	forwarded := "79.109.178.183"
-	remoteip  := "46.105.196.10:43025"
+	remoteip  := getip("46.105.196.10:13089")
 	
-	//operaciones para el nombre de fichero
-	archivo 	:= strings.Split(namefile, "/")
-	streamname  := strings.Split(archivo[4], ".")
+	createStats(namefile, agent, forwarded, remoteip)
+}
+
+func createStats(namefile, agent, forwarded, remoteip string){
+	userAgent := map[string]string {"win":"Windows", "mac":"Mac OS X", "and":"Android", "lin":"Linux"}
+	var existe bool
+	username  := "isaac"
+	var stream string
+	//operaciones sobre el namefile
+	fmt.Sscanf(namefile, "/var/segments/live/%s", &stream)
+	streamname := strings.Split(stream, ".")
 	namefile = username+"-"+streamname[0]
-	//operaciones para la ip remota
-	ip 			:= strings.Split(remoteip, ":")
 	//operaciones para el user agent
 	for key, value := range userAgent{
 		if strings.Contains(agent, value){
@@ -28,10 +32,27 @@ func main(){
 			existe = false
 		}
 	}
+	//Cuando el forwarded está vacio
+	if forwarded == "" {
+		forwarded = remoteip
+	}
 	fmt.Printf("%s\n", namefile)	//Nombre del fichero
 	fmt.Printf("%s\n", forwarded)	//IP forwarded
-	fmt.Printf("%s\n", ip[0])		//IP remota
-	if existe == false{				//Agent User no find
+	fmt.Printf("%s\n", remoteip)	//IP remota
+	if existe == false {			//Agent User no find
 		fmt.Printf("other\n")
 	}
+}
+
+func getip(pseudoip string) string {
+	var res string
+	if strings.Contains(pseudoip, "]:") {
+	  part := strings.Split(pseudoip, "]:")
+	  res = part[0]
+	  res = res[1:]
+	}else{
+	  part := strings.Split(pseudoip, ":")
+	  res = part[0]
+	} 
+	  return res
 }
