a2762c06588fef7c582a8c55acaf75cdd447dd33
Isaac <isaac@todostreaming.es>
2016-10-27 01:15:42 +0200

    Solucionado problema XML publish

--- test.go
+++ test.go
@@ -5,14 +5,13 @@ import (
 	"fmt"
 	"net/http"
 	"io/ioutil"
-	"strings"
 )
 
 func main(){
 		type Encoder struct {
-			Ip			[]string  `xml:"address"`
-			Time 		[]string  `xml:"time"`
-			Flash 		[]string  `xml:"flashver"`
+			Ip			string  `xml:"address"`
+			Time 		string  `xml:"time"`
+			Publish 	int  	`xml:"publishing"`
 		}
 		type Result struct {
 			Nombre 		[]string `xml:"server>application>live>stream>name"`
@@ -24,40 +23,29 @@ func main(){
 			Acodec 		[]string `xml:"server>application>live>stream>meta>audio>codec"`
 			EncoderList []Encoder `xml:"server>application>live>stream>client"`
 		}
-		resp, err := http.Get("http://panel.cdnstreamserver.com:8080/stats")
+		resp, err := http.Get("http://37.187.152.10:8080/stats")
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
 		body, err := ioutil.ReadAll(resp.Body)
 		resp.Body.Close()
-		// ZONA XML DE SERVIDOR
 		var r Result
 		err = xml.Unmarshal(body, &r)
 		if err != nil {
 			fmt.Printf("xml read error: %s", err) 
 			return
 		}
-		for k, _ := range r.Nombre {
+		/*for k, _ := range r.Nombre {
 			fmt.Printf("%s-%s-%s-%s-%s-%s-%s\n", r.Nombre[k], r.Bw_in[k], r.Width[k], r.Height[k], r.Frame[k], r.Vcodec[k], r.Acodec[k])
 		}
-
-		for key, val := range r.EncoderList {
-			if strings.Contains(val.Flash[key], "FMLE"){
-				fmt.Printf("%s\n", val.Flash)
-			}
-		}
-		/*
-		c := Encoder{}
-		err2 := xml.Unmarshal([]byte(body), &c)
-		if err2 != nil {
-			fmt.Printf("xml read error: %s", err2) 
-			return
-		}
-		for k, _ := range c.Ip {
-			if strings.Contains(c.Flash[k], "FMLE"){
-				fmt.Printf("%s-%s\n", c.Ip[k], c.Time[k])
-			}
-		}
 		*/
+		for _, val := range r.EncoderList {
+			for k, _ := range r.Nombre {
+				if val.Publish == 1 {
+					fmt.Printf("%s-%s-%s-%s-%s-%s-%s-%s-%s\n", r.Nombre[k], r.Bw_in[k], r.Width[k], r.Height[k], r.Frame[k], r.Vcodec[k], r.Acodec[k], val.Ip, val.Time)
+				}
+				
+			}
+		}
 }
