3773e9bf73d6a54015914227ccb8fbea01896425
Isaac ML <isaac@todostreaming.es>
2016-10-27 11:13:56 +0200

    XML publish parsing finish

--- test.go
+++ test.go
@@ -8,22 +8,26 @@ import (
 )
 
 func main(){
-		type Encoder struct {
+		type Client struct {
 			Ip			string  `xml:"address"`
 			Time 		string  `xml:"time"`
 			Publish 	int  	`xml:"publishing"`
 		}
-		type Result struct {
-			Nombre 		[]string `xml:"server>application>live>stream>name"`
-			Bw_in  		[]string `xml:"server>application>live>stream>bw_in"`
-			Width  		[]string `xml:"server>application>live>stream>meta>video>width"`
-			Height 		[]string `xml:"server>application>live>stream>meta>video>height"`
-			Frame  		[]string `xml:"server>application>live>stream>meta>video>frame_rate"`
-			Vcodec 		[]string `xml:"server>application>live>stream>meta>video>codec"`
-			Acodec 		[]string `xml:"server>application>live>stream>meta>audio>codec"`
-			EncoderList []Encoder `xml:"server>application>live>stream>client"`
+		type Stream struct {
+			Nombre 		string `xml:"name"`
+			Bw_in  		string `xml:"bw_in"`
+			Width  		string `xml:"meta>video>width"`
+			Height 		string `xml:"meta>video>height"`
+			Frame  		string `xml:"meta>video>frame_rate"`
+			Vcodec 		string `xml:"meta>video>codec"`
+			Acodec 		string `xml:"meta>audio>codec"`
+			ClientList  []Client `xml:"client"`
 		}
-		resp, err := http.Get("http://37.187.152.10:8080/stats")
+		type Result struct {
+			Stream		[]Stream  `xml:"server>application>live>stream"`
+			
+		}
+		resp, err := http.Get("http://panel.cdnstreamserver.com:8080/stats")
 		if err != nil {
 			fmt.Println(err)
 			return
@@ -36,16 +40,11 @@ func main(){
 			fmt.Printf("xml read error: %s", err) 
 			return
 		}
-		/*for k, _ := range r.Nombre {
-			fmt.Printf("%s-%s-%s-%s-%s-%s-%s\n", r.Nombre[k], r.Bw_in[k], r.Width[k], r.Height[k], r.Frame[k], r.Vcodec[k], r.Acodec[k])
-		}
-		*/
-		for _, val := range r.EncoderList {
-			for k, _ := range r.Nombre {
-				if val.Publish == 1 {
-					fmt.Printf("%s-%s-%s-%s-%s-%s-%s-%s-%s\n", r.Nombre[k], r.Bw_in[k], r.Width[k], r.Height[k], r.Frame[k], r.Vcodec[k], r.Acodec[k], val.Ip, val.Time)
+		for _, v := range r.Stream {
+			for _, v2 := range v.ClientList {
+				if v2.Publish == 1 {
+					fmt.Printf("%s-%s-%s-%s-%s-%s-%s-%s-%s\n", v.Nombre, v.Bw_in, v.Width, v.Height, v.Frame, v.Vcodec, v.Acodec, v2.Ip, v2.Time)
 				}
-				
 			}
 		}
 }
